127c6c86ca40349cd353b01ef3453da6b3de8774
roman <roman.a.shushakov1@gmail.com>
2026-01-14 11:19:28 +0100

    added

--- scripts/33_export_billing_v2.sh
+++ scripts/33_export_billing_v2.sh
@@ -40,54 +40,59 @@ mkdir -p "$(dirname "$OUT")"
 
 # We write CSV with header.
 jq -r \
-  --arg price_cpu_hr "$PRICE_CPU_HR" \
-  --arg price_gpu_hr "$PRICE_GPU_HR" \
-  --arg price_billing_hr "$PRICE_BILLING_HR" '
-  def n: if . == null then 0 else . end;
-  def to_num: try tonumber catch 0;
+  --arg price_cpu_hr     "${PRICE_CPU_HR:-0}" \
+  --arg price_gpu_hr     "${PRICE_GPU_HR:-0}" \
+  --arg price_billing_hr "${PRICE_BILLING_HR:-0}" '
+  def to_num:
+    if . == null or . == "" then 0
+    else (try tonumber catch 0)
+    end;
 
-  def job_is_top_level:
-    (.job_id | tostring | test("\\.") ) | not;
-
-  def norm_account:
-    if (.account | tostring | length) == 0 then "unknown" else .account end;
-
-  def sec_to_hr: (. / 3600);
+  def sec_to_hr($sec):
+    (($sec | to_num) / 3600);
 
   def cost($sec; $price_hr):
-    (sec_to_hr($sec) * ($price_hr|to_num));
+    (sec_to_hr($sec) * ($price_hr | to_num));
 
-  (["account","user","jobs","cpu_hours","gpu_hours","billing_hours","cpu_cost","gpu_cost","billing_cost","total_cost"] | @csv),
-  (
-    .jobs
-    | map(select(job_is_top_level))
-    | map(.account = norm_account)
-    | group_by(.account, .user)
-    | map({
-        account: (.[0].account),
-        user: (.[0].user),
-        jobs: (length),
+  def csv_escape:
+    tostring
+    | gsub("\""; "\"\"")
+    | "\"" + . + "\"";
 
-        cpu_sec:     (map(.cpu_seconds     | n) | add),
-        gpu_sec:     (map(.gpu_seconds     | n) | add),
-        billing_sec: (map(.billing_seconds | n) | add)
-      })
-    | map(. + {
-        cpu_hours:     (sec_to_hr(.cpu_sec)),
-        gpu_hours:     (sec_to_hr(.gpu_sec)),
-        billing_hours: (sec_to_hr(.billing_sec)),
+  # header
+  [
+    "user","account","job_id",
+    "cpu_sec","gpu_sec","billing_sec",
+    "cpu_hours","gpu_hours","billing_hours",
+    "cpu_cost","gpu_cost","billing_cost",
+    "total_cost"
+  ] | @csv,
 
-        cpu_cost:     (cost(.cpu_sec;     $price_cpu_hr)),
-        gpu_cost:     (cost(.gpu_sec;     $price_gpu_hr)),
-        billing_cost: (cost(.billing_sec; $price_billing_hr))
-      })
-    | map(. + { total_cost: (.cpu_cost + .gpu_cost + .billing_cost) })
-    | sort_by(-.total_cost)
-    | .[]
-    | [ .account, .user, .jobs,
-        (.cpu_hours|tostring), (.gpu_hours|tostring), (.billing_hours|tostring),
-        (.cpu_cost|tostring), (.gpu_cost|tostring), (.billing_cost|tostring),
-        (.total_cost|tostring)
+  (.jobs[]?
+    | {
+        user: (.user // ""),
+        account: (.account // ""),
+        job_id: (.job_id // ""),
+        cpu_sec: (.cpu_seconds // 0),
+        gpu_sec: (.gpu_seconds // 0),
+        billing_sec: (.billing_seconds // 0)
+      }
+    | .cpu_hours     = sec_to_hr(.cpu_sec)
+    | .gpu_hours     = sec_to_hr(.gpu_sec)
+    | .billing_hours = sec_to_hr(.billing_sec)
+
+    | .cpu_cost     = cost(.cpu_sec; $price_cpu_hr)
+    | .gpu_cost     = cost(.gpu_sec; $price_gpu_hr)
+    | .billing_cost = cost(.billing_sec; $price_billing_hr)
+
+    | .total_cost = (.cpu_cost + .gpu_cost + .billing_cost)
+
+    | [
+        .user, .account, .job_id,
+        (.cpu_sec|to_num), (.gpu_sec|to_num), (.billing_sec|to_num),
+        (.cpu_hours), (.gpu_hours), (.billing_hours),
+        (.cpu_cost), (.gpu_cost), (.billing_cost),
+        (.total_cost)
       ]
     | @csv
   )
